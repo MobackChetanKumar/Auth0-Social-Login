e775b70c092e172554f9091b8c9717cea03c5179
Chetan Kumar Moback <chetank@moback.com>
2022-02-25 16:16:34 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// Import the AuthService type from the SDK
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Auth0-Social-login';
+  userLoggedIn = false;
+  constructor(public auth: AuthService) {
+    this.hideLoginButton();
+  }
+  loginWithRedirect(): void {
+    // Call this to redirect the user to the login page
+    this.auth.loginWithRedirect();  
+    this.hideLoginButton(); 
+  }
+  hideLoginButton() {
+    this.auth.user$.subscribe((user) => 
+    {
+      if (user) {       
+        this.userLoggedIn = true;
+      }  else {
+        this.userLoggedIn = false;
+      }
+    });
+    
+  }
+  logout(): void {
+    this.userLoggedIn = false;
+    // Call this to log the user out of the application
+    this.auth.logout({ returnTo: window.location.origin });
+  }
+  title = 'Welcome Kornferry D2T';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-ujzgl0vq.us.auth0.com',
+      clientId: '74yO5k3rBtwyGewCDtbxm2Ub7gNjXR3n',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
